6072c4483f328269ff2d71a247c8d8010e3f6fec
JohnAlexanderG <johnalexand3rg@gmail.com>
2023-09-23 20:58:27 -0500

    style(imports) - clean necessary imports

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_app_imagineapps/api/services/auth_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
